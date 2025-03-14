6641a318f04a11496e5f3415eba2152c43dabdbb
simoneburrai <burrai.s98@gmail.com>
2025-03-14 14:03:05 +0100

    feat: created html element, for every element of the API array created html element w/json classes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+// API link 
 const uri = "https://lanciweb.github.io/demo/api/pictures/"
+
+// htmlElements 
+const containerElement = document.getElementById("cards-container");
+console.log(containerElement)
+
+
 
 
 axiosCall(uri);
@@ -9,10 +16,29 @@ axiosCall(uri);
 
 function axiosCall (apiLink){
     axios.get(apiLink)
+
         .then (function (response) {
             const elements = response.data;
             console.log(elements)
+            console.log(elements[0])
+            for(i=0; i<elements.length; i++){
+                containerElement.innerHTML += createHTMLElement(elements[i])
+            }
         })
 }
 
 
+function createHTMLElement (obj){
+    htmlElement = `
+    <div class="photo-card">
+                <img src="img/pin.svg" alt="" class="pin">
+                <div class="photo-container">
+                    <img src="${obj.url}" alt=" ${obj.title}">
+                </div>
+                <div class="photo-description">
+                    ${obj.title}
+                </div>
+            </div>
+    `
+    return htmlElement;
+}
